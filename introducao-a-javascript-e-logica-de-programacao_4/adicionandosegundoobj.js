8b3a6d0ccf117133a9c5873658ce86cec6b9f90e
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2020-10-28 19:22:51 -0300

    adicionando segundo objeto e exibindo valores da chave do objeto 1

---
+++ introducao-a-javascript-e-logica-de-programacao_4/adicionandosegundoobj.js
@@ -0,0 +1,24 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: 'Sim'
+};
+
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: 'Sim'
+};
+
+
+for (let i in info) {
+    if (i === "recorrente") {
+        info2[i] = "Ambos recorrentes";
+    } else {
+        info2[i] = info[i] + " e " + info2[i]; // "" + ""
+    }
+    console.log(info2[i]);
+}
