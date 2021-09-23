b6cfd51e4bd5663d58af4776f09dc08eb2a50afb
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-09-23 19:17:27 -0300

    Exercicio 3, 4 e 5 resolvidos

--- exercises/26. Introduction to Web Development with NodeJS/My scripts/imc.js
+++ exercises/26. Introduction to Web Development with NodeJS/My scripts/imc.js
@@ -1,6 +1,30 @@
-function calcularIMC(peso, altura) {
-  return peso / (altura * altura);
+const readline = require('readline-sync');
+
+const peso = readline.questionInt('What`s is your weight?');
+const altura = readline.questionFloat('What`s is your height?'); 
+
+
+function calcularIMC() { 
+  return (peso / (altura * altura)).toFixed(2);
+}
+
+function categoriaIMC(imc){
+   if(imc < 18.5) {
+      console.log("Abaixo do peso (magreza)");
+   }else if(imc >= 18.5 && imc <= 24.9){
+    console.log("Peso normal");
+   }else if(imc >= 25.0 && imc <= 29.9){
+    console.log("Acima do peso (sobrepeso)");
+   }else if(imc >= 30.0 && imc <= 34.9){
+    console.log("Obesidade grau I");
+   }else if(imc >= 35.0 && imc <= 39.9){
+    console.log("Obesidade grau II");
+   } else{
+    console.log("Obesidade grau III e IV")
+}
 }
 
 
-console.log(calcularIMC(90, 1.90));
+
+console.log(calcularIMC());
+console.log(categoriaIMC(calcularIMC()));
