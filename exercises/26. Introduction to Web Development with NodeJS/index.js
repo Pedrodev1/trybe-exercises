f5280f4bd588579118a31f4a516c54aa2d95b90b
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-09-30 12:09:52 -0300

    Atualizando script

--- exercises/26. Introduction to Web Development with NodeJS/index.js
+++ exercises/26. Introduction to Web Development with NodeJS/index.js
@@ -1,17 +1,17 @@
 const readline = require('readline-sync');
 const ArrObj = [
   { name: "Calcular IMC", script: "./imc.js"}, { name: "Sorteio", script: "./sorteio.js"},
-  { name: "Calcular velocidade", script: "./velocidade.js"}
+  { name: "Calcular velocidade", script: "./velocidade.js"}, { name: "Calcular fatorial", script: "./fatorial.js"}, { name: "Calcular Fibonacci", script: "./fibonacci.js"}
 ]
 
 const newArray = ArrObj.map((value, index) => `${index + 1} : ${value.name}`)
 
 
-newArray.unshift('Escolha um script correspondente ');
+
 const escolheScript = readline.question(newArray.join('\n'));
 
 
-const qualScript = readline.questionInt('Qual script voce quer executar ?');
+const qualScript = readline.questionInt('Qual script voce quer executar? ');
 const selectedScript = ArrObj[qualScript - 1];
 
 require(selectedScript.script);
